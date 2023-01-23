38585c0d84a1e6b1f8c11525fe3f82b98e68ae26
Alan Liu <b05901017@ntu.edu.tw>
2023-01-23 10:16:00 +0800

    add respective sys argv

--- get_video_from_yt.py
+++ get_video_from_yt.py
@@ -1,8 +1,12 @@
 # python -m pip install --upgrade pytube
 from pytube import YouTube
-
+import sys
 
 def donwload(url, filename="test.mp4"):
     selected_video = YouTube(url)
     audio = selected_video.streams.filter(only_audio=True, file_extension='mp4').first()
     audio.download(output_path=".", filename=filename)
+
+
+if __name__ == "__main__":
+    donwload(sys.argv[1])
--- video2script.py
+++ video2script.py
@@ -4,13 +4,34 @@ import os
 import glob
 import whisper
 import json
+import sys
 
-
-def split_video(vid, language='English', output="script.json", model="base"):
+def split_video(vid, language='English', output="script.json", model="base", prune=True):
    language = language 
    translate = False 
 
    model = whisper.load_model(model)
    result = model.transcribe(vid)
+
+
+   if prune:
+      result_concise = {
+         'text-length': len(result['text'].split()),
+         'segments': []
+      }
+      for r in result['segments']:
+         result_concise['segments'].append(
+            {
+               "start": r["start"],
+               "end": r["end"],
+               "text": r["text"],
+         },
+         )
+      result = result_concise
+
    with open(output, "w") as outfile:
       json.dump(result, outfile, indent = 4) 
+
+
+if __name__ == "__main__":
+   split_video(sys.argv[1], language='English', output="script.json", model="base", prune=True)
